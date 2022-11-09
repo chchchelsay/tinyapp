3faf4c4b57b2b4e99084745657a0246c53b1567e
Chelsea Dwarika <chelseakristina@live.com>
2022-11-09 00:10:00 +0000

    add routes for /urls, /urls/:id, add header

--- express_server.js
+++ express_server.js
@@ -26,8 +26,17 @@ app.get("/hello", (req, res) => {
 //route handler for /urls
 app.get("/urls", (req, res) => {
    const templateVars = { urls: urlDatabase };
-  res.render('pages/urls_index', templateVars);
+  res.render('urls_index', templateVars);
 });
+
+//route handler for /urls/:id
+//route path: /urls/:id
+//request URL: http://localhost:8080/urls/b2xVn2
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {id: "b2xVn2", longURL: "http://www.lighthouselabs.ca"};
+  res.render('urls_show', templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
