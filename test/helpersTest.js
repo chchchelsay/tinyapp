ca590410959e3bd3c7fada9954df79f0b58692a3
Chelsea Dwarika <chelseakristina@live.com>
2022-11-15 15:44:21 +0000

    tests work!

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { getUserByEmail} = require('../helpers.js');
+const { getUserByEmail, urlsForUser} = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -20,5 +20,41 @@ describe('getUserByEmail', function() {
     const expectedUserID = "userRandomID";
     assert.equal(user.id, expectedUserID);
   });
+
+  it('should return null if email is empty', function() {
+    const user = getUserByEmail("", testUsers);
+    const expectedUserID = null;
+    assert.equal(user, expectedUserID);
+  });
+
+  it('should return null if email is invalid', function() {
+    const user = getUserByEmail("chels@example.com", testUsers);
+    const expectedUserID = null;
+    assert.equal(user, expectedUserID);
+  });  
 });
 
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "userRandomID2",
+  },
+};
+describe('urlsForUser', function() {
+  it('should return an object with all urls for a given user ID', function() {
+    const expected = {
+      "b2xVn2": {
+        longURL: "http://www.lighthouselabs.ca",
+        userID: "userRandomID",
+      },
+    };
+
+    const actual = urlsForUser('userRandomID', urlDatabase);
+
+    assert.deepEqual(expected, actual);
+  });
+});
